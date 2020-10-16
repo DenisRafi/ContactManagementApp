fc61ed1ca47b8cd92fba5b24c3432e4da25a532f
Denis Rafi <62852838+DenisRafi@users.noreply.github.com>
2020-10-16 03:09:36 +0100

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ContactManagementApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtSearch_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13)
+                phoneBooksBindingSource.Filter = $"FullName '*{txtSearch.Text}*' or Mobile='{txtSearch.Text}'";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (File.Exists($"{Application.StartupPath}/data.dat"))
+                database.ReadXml($"{Application.StartupPath}/data.dat");
+        }
+
+        private void phoneBooksBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            phoneBooksBindingSource.EndEdit();
+            database.WriteXml($"{Application.StartupPath}/data.dat");
+            MessageBox.Show("Your data has been successfully saved.", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
